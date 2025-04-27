8bf646a46f4beec9dd82f42c64537cc07a41c7c5
Evgenbek <Evgenbek@yandex.ru>
2025-04-27 16:07:39 +0300

    chore: fix file index.html - delete link & adds in fileindex.js import ./index.css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
@@ -26,10 +26,10 @@ module.exports = {
         test: /\.html$/i,
         loader: 'html-loader',
       },
-    //   {
-    //     test: /\.css$/i,
-    //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
-    //   },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({
