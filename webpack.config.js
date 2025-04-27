ee717452b28bd2790a581856d06b94cb9a93ae64
Evgenbek <Evgenbek@yandex.ru>
2025-04-27 13:49:54 +0300

    feat: adds babel-loader & adds file webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  //   resolve: {
+  //     extensions: ['.ts', '.js'],
+  //     extensionAlias: {
+  //       '.js': ['.js', '.ts'],
+  //       '.cjs': ['.cjs', '.cts'],
+  //       '.mjs': ['.mjs', '.mts'],
+  //     },
+  //   },
+  module: {
+    rules: [
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+    //   {
+    //     test: /\.([cm]?ts)$/,
+    //     loader: 'ts-loader',
+    //   },
+    //   {
+    //     test: /\.css$/i,
+    //     use: [MiniCssExtractPlugin.loader, 'css-loader'],
+    //   },
+    ],
+  },
+//   plugins: [new HtmlWebpackPlugin({
+//     template: './src/index.html',
+//   }), new MiniCssExtractPlugin()],
+};
